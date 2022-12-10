e665fea9b2fe38d6eaa1b8a58e10865da571b616
Piotr Pakuła <mpa.controllers.ppakua@gmail.com>
2022-12-10 15:16:04 +0100

    Prepared data module.

--- app/src/main/java/com/example/currencycalculator/MainActivity.kt
+++ app/src/main/java/com/example/currencycalculator/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.currencycalculator
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.data.TestData
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val testData = TestData()
     }
 }
